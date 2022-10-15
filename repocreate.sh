cab6f40cb007d6f2556babcf4e10cd0ddadaeee3
d1lanX <rodriguezdylan58@gmail.com>
2022-10-15 10:34:14 -0500

    fix: se arregla error de git  al crear un repo sin archivos iniciales

--- repocreate.sh
+++ repocreate.sh
@@ -32,23 +32,24 @@ while [ "$readme" != "y" ] && [ "$readme" != "n" ]; do
     read -r readme
 done
 
+git init 
 
 if [ "$readme" = "y" ]; then
-    touch README.md && echo "# $nombreRepo" > README.md
+    echo "\n"
+    touch README.md && echo "# $nombreRepo" > README.md;
+    echo > .gitignore
+    git add .
+    git commit -m "feat: Commit inicial de estructura de proyecto"
     echo "\n${GREEN}Archivos creados${ENDCOLOR}\n"
 else
     echo "\n${RED}README.md, gitignore no fueron creados${ENDCOLOR}\n"
 fi
 
 
-git init 
-git add .
-git commit -m "feat: Commit inicial de estructura de proyecto"
-
 echo -n "\n${GREEN}Usuario de Github:${ENDCOLOR} "
 read username
 
-echo "Si no tienes un token de acceso a tu cuenta de Github..."
+echo "\nSi no tienes un token de acceso a tu cuenta de Github..."
 echo "este es el momento para obtenerlo https://tinyurl.com/44xjrs79\n"
 sleep 2
 
@@ -58,10 +59,10 @@ URL=https://github.com/$username/$nombreRepo.git
 
 git remote add origin $URL
 
-git push --set-upstream origin main
-
-git push -u origin
-
+if [ "$readme" = "y" ]; then
+    git push --set-upstream origin main
+    git push -u origin
+fi
 
 # Borro los archivos locales
 cd ..
